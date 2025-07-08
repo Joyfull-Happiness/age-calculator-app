fac7835e1d8431be8e370836b680045ab0b663f6
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-07-08 09:20:32 -0700

    updated css

---
+++ age-calculator-app-main/api.js
@@ -0,0 +1,6 @@
+const bdayIn = document.getElimentById("birthday");
+const btnIn = document.getElimentById("btn");
+const publishAge = document.getElimentById("result");
+function calcuAge(bdayIn){
+    const bday
+}
