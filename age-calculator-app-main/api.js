7304220dbb6558fd55b383ae8ee4ad380cdc1b05
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-07-08 10:31:08 -0700

    updated js

--- age-calculator-app-main/api.js
+++ age-calculator-app-main/api.js
@@ -1,6 +1,18 @@
 const bdayIn = document.getElimentById("birthday");
 const btnIn = document.getElimentById("btn");
 const publishAge = document.getElimentById("result");
-function calcuAge(bdayIn){
-    const bday
+
+function calcuAge(bdayIn) {
+  const bdayFromUser = bdayIn.value;
+  if (bdayFromUser == "") {
+    alert("Enter a valid bday");
+  } else {
+    const age = getAge(bdayFromUser);
+    publishAge.innerText = 'Now age is ${age} ${age > 1?"years: year"} old';
+  }
+}
+
+function getAge(bdayFromUser) {
+  const Tday = new Date();
+  const born = new Date(bdayFromUser);
 }
